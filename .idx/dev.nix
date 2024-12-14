c8d416fa62ff2ce7844957c01997098c7cfcca4c
bestedu58 <bestedu58@gmail.com>
2024-12-14 13:40:21 +0000

    add sqlite and idx file

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,10 +5,9 @@
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs_20
-    pkgs.yarn
+    pkgs.nodejs_22
+    pkgs.sqlite
     pkgs.nodePackages.pnpm
-    pkgs.bun
   ];
   # Sets environment variables in the workspace
   env = {};
